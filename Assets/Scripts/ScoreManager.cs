1be6c207e3720d39d8430aa7e50e707d8e7c3697
almahahahhaa <869687665m1@gmail.com>
2026-02-05 20:09:17 +0500

    added score and tileset assets

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+
+    public TMP_Text scoreText;
+    public int score = 0;
+
+    void Awake()
+    {
+        Instance = this;
+        UpdateUI();
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
